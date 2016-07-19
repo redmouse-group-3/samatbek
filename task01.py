61c495fcb73b9cab0d04b1505d6f34854f32f5dd
samatbek <samatbek.osmonov@gmail.com>
2016-07-19 19:04:50 +0600

    Add list comprehensions and functions

--- task01.py
+++ task01.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 x = int(raw_input("Введите число от 1 до 9: "))
 
-if 1 <= x <= 3:
-    s = raw_input("Введите строку: ")
-    n = int(raw_input("Введите число повторов строки: "))
-    count1 = 0
-    while count1 < n:
-        print s
-        count1 += 1
-elif 4 <= x <= 6:
-    m = int(raw_input("В какую степеть восвести ваше число? "))
-    print x ** m
-elif 7 <= x <= 9:
-    count2 = 1
-    while count2 <= 10:
-        print x + count2
-        count2 += 1
-else:
-    print('Ошибка ввода')
+
+def check_conditions(x):
+    if 1 <= x <= 3:
+        string = raw_input("Введите строку: ")
+        number = int(raw_input("Введите число повторов строки: "))
+        print ((string + "\n") * number)
+    elif 4 <= x <= 6:
+        power = int(raw_input("В какую степеть восвести ваше число? "))
+        print x ** power
+    elif 7 <= x <= 9:
+        output_numbers = [i for i in range(x + 1, x + 11)]
+        for i in output_numbers:
+            print i
+    else:
+        print('Ошибка ввода')
+
+check_conditions(x)
