9fc13ff298bdfd1b0922accdfc053f6c8b45a0c6
samatbek <samatbek.osmonov@gmail.com>
2016-07-22 00:45:15 +0600

    Update

---
+++ hw04_task01/pack_task01/__init__.py
@@ -0,0 +1,5 @@
+# outer __init__.py
+from func_1_to_3 import func_1_to_3
+from func_4_to_6 import func_4_to_6
+from func_7_to_9 import func_7_to_9
+from func_error import func_error
--- hw04_task01/task01.py
+++ hw04_task01/task01.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
-from pack_task01 import if_module01
+import sys
+
+sys.path.append('home/python/samatbek/hw04_task01')
+
+import pack_task01
 
 x = int(raw_input("Введите число от 1 до 9: "))
 
 if 1 <= x <= 3:
-    print if_module01.func_1_to_3()
+    print pack_task01.func_1_to_3()
 elif 4 <= x <= 6:
-    print if_module01.func_4_to_6(x)
+    print pack_task01.func_4_to_6(x)
 elif 7 <= x <= 9:
-    print if_module01.func_7_to_9(x)
+    print pack_task01.func_7_to_9(x)
 else:
-    print if_module01.func_error()
+    print pack_task01.func_error()
 
 
