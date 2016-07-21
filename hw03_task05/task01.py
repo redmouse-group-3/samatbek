8dda08417cbd239387886e65c3a685328fdb564b
samatbek <samatbek.osmonov@gmail.com>
2016-07-21 18:03:21 +0600

    Add module

--- hw03_task05/task01.py
+++ hw03_task05/task01.py
@@ -1,33 +1,15 @@
 # -*- coding: utf-8 -*-
-def func_1_to_3():
-    string = raw_input("Введите строку: ")
-    number = int(raw_input("Введите число повторов строки: "))
-    print ((string + "\n") * number)
-
-
-def func_4_to_6():
-    power = int(raw_input("В какую степеть восвести ваше число? "))
-    print x ** power
-
-
-def func_7_to_9():
-    output_numbers = [i for i in range(x + 1, x + 11)]
-    for i in output_numbers:
-        print i
-
-
-def func_error():
-    print('Ошибка ввода')
+import if_module01
 
 x = int(raw_input("Введите число от 1 до 9: "))
 
 if 1 <= x <= 3:
-    func_1_to_3()
+    print if_module01.func_1_to_3()
 elif 4 <= x <= 6:
-    func_4_to_6()
+    print if_module01.func_4_to_6(x)
 elif 7 <= x <= 9:
-    func_7_to_9()
+    print if_module01.func_7_to_9(x)
 else:
-    func_error()
+    print if_module01.func_error()
 
 
