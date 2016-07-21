ec3c11a7cb28be1f006db947234ca4c0ef04391f
samatbek <samatbek.osmonov@gmail.com>
2016-07-22 01:04:25 +0600

    Update task01

--- hw04_task01/task01.py
+++ hw04_task01/task01.py
@@ -7,12 +7,12 @@ import pack_task01
 
 x = int(raw_input("Введите число от 1 до 9: "))
 
-if 1 <= x <= 3:
-    print pack_task01.func_1_to_3()
-elif 4 <= x <= 6:
-    print pack_task01.func_4_to_6(x)
-elif 7 <= x <= 9:
+if 9 >= x >= 7:
     print pack_task01.func_7_to_9(x)
+elif x >= 4:
+    print pack_task01.func_4_to_6(x)
+elif x >= 1:
+    print pack_task01.func_1_to_3()
 else:
     print pack_task01.func_error()
 
