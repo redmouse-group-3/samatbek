fd8df8271ccebb34365ba3340bad919666a0151d
samatbek <samatbek.osmonov@gmail.com>
2016-07-30 16:58:11 +0600

    Add home work 4 task 1

---
+++ hw04_task04/task01.py
@@ -0,0 +1,44 @@
+import random
+import re
+from sys import exit
+
+
+def get_rand_words():
+    words = (random.sample(['samsung', 'apple', 'motorolla', 'lenovo', 'lg', 'meizu', 'xiaomi', 'oneplus'], 8))
+    return ' '.join(words)
+
+
+def get_first_word(string):
+    return str(re.findall(r'^\w+', string)[0])
+
+
+def get_last_word(string):
+    return str(re.findall(r'\w+$', string)[0])
+
+
+def reverse_first(string, word):
+    return re.sub(r'^\w+', word[::-1], string)
+
+
+def reverse_last(string, word):
+    return re.sub(r'\w+$', word[::-1], string)
+
+
+text = get_rand_words()
+
+print('text before: %s' % text)
+
+user_word = raw_input("Enter word: ")
+
+if user_word == get_first_word(text):
+    text = reverse_first(text, user_word)
+elif user_word == get_last_word(text):
+    text = reverse_last(text, user_word)
+else:
+    print "Invalid input!"
+    exit(0)
+
+print('text after:  %s' % text)
+
+
+
