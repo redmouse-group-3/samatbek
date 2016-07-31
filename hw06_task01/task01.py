c5cf3be2ed05a51d948a784516f4e73aaafeefb2
samatbek <samatbek.osmonov@gmail.com>
2016-07-31 22:43:26 +0600

    Add home work 5 task 1

--- hw06_task01/task01.py
+++ hw06_task01/task01.py
@@ -1,32 +1,50 @@
 # -*- coding: utf-8 -*-
-class IfNum:
-    x = 0
+from abc import ABCMeta, abstractmethod, abstractproperty
 
-    def __init__(self, x):
+
+class AbstractAttribute:
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def __init__(self, x=0):
         self.x = x
-        if 9 >= self.x >= 7:
-            print self.func_7_to_9()
-        elif self.x >= 4:
-            print self.func_4_to_6()
-        elif self.x >= 1:
-            print self.func_1_to_3()
-        else:
-            print self.func_error()
 
-    def func_1_to_3(self):
+
+class Methods:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def func_1_to_3():
         string = raw_input("Введите строку: ")
         number = int(raw_input("Введите число повторов строки: "))
         return (string + "\n") * number
 
-    def func_4_to_6(self):
+    @staticmethod
+    def func_4_to_6(x):
         power = int(raw_input("В какую степеть восвести ваше число? "))
-        return self.x ** power
+        return x ** power
 
-    def func_7_to_9(self):
-        return [i for i in range(self.x + 1, self.x + 11)]
+    @staticmethod
+    def func_7_to_9(x):
+        return [i for i in range(x + 1, x + 11)]
 
-    def func_error(self):
+    @staticmethod
+    def func_error():
         return 'Ошибка ввода'
+
+
+class IfNum(AbstractAttribute, Methods):
+    def __init__(self, x):
+        super(IfNum, self).__init__(x)
+        if 9 >= self.x >= 7:
+            print self.func_7_to_9(self.x)
+        elif self.x >= 4:
+            print self.func_4_to_6(self.x)
+        elif self.x >= 1:
+            print self.func_1_to_3()
+        else:
+            print self.func_error()
 
 
 x = int(raw_input("Введите число от 1 до 9: "))
